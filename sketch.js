b17a80018cc3df9a9dd9c1bef8b536d70194c067
AnnaSpada <spada.anna12@gmail.com>
2023-01-19 22:50:50 +0100

    new project

--- sketch.js
+++ sketch.js
@@ -1,60 +1,97 @@
-const myPalette = ['#FF99C8', '#FCF6BD', '#D0F4DE','#A9DEF9', '#E4C1F9']
-const myPalette2 = ['#FF9888', '#FCF888', '#D0F888','#A98888', '#E4C777']
+pioggia=[]
+colori=["#3066BE","#119DA4","#6D9DC5","#80DED9","#AEECEF"]
 
+function centerCanvas() {
+  let x = (windowWidth - width) / 3;
+  let y = (windowHeight - height) / 3;
+  cnv.position(x, y);
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); 
-  
+  cnv = createCanvas(windowWidth, windowHeight);
+  centerCanvas();
+	background('#f0ead6');
 }
 
-
-function draw() {    
-  noLoop();
-} 
-      
-
-function mouseClicked() {
-  background(0)
-  let listaRaggi = [width/50,
-                    width/80,
-                    width/100,
-                    width/120]
-
-  let raggio = random(listaRaggi);
+function draw() {
+	coloresfondo=color('#fffefa');
+	coloresfondo.setAlpha(10);
+	background(coloresfondo);
   
-    for (let i = 1; i <= width/raggio*2 ; i+=2) {
-      for(let b = 1; b <= height/raggio*2 ; b+=2){
+  if(frameCount%2==0){
+    pioggia.push(new drip(random(width), random(height), random(10, 200)))
+  }
 
-      let x = i * raggio/2;
-      let y = b * raggio/2;
+  for(let i=pioggia.length-1; i>=0; i--){
+    pioggia[i].move();
+    pioggia[i].show();
+  }
+}
 
-     
-      col = random(myPalette)
-      fill(color(col));
-      circle(x,y,raggio);
-     }
+class pointer{
+  constructor(rad, acc, finalSize){
+    this.dist=1;
+    this.rad=rad;
+    this.speed=0;
+    this.acc=acc;
+    this.pos=createVector(0, 0);
+    this.finalSize=finalSize;
+    this.downSpeed=createVector(0, 0.5);
+    this.downAcc=createVector(0, 0.05+this.acc/500);
   }
   
+  move(){
+    if(this.dist<=this.finalSize){
+    this.speed+=this.acc;
+    this.dist+=this.speed;
+    this.pos=createVector(cos(this.rad)*this.dist, sin(this.rad)*this.dist);
+    } else{
+      this.downSpeed.add(this.downAcc);
+      this.pos.add(this.downSpeed);  
+    }
+  }
 }
 
-function mouseMoved() {   
-  if(mouseX>100)
-  {
-     fill(255,0,0);
-  } else {
-     fill(200);
+
+class drip{
+  constructor(x, y, extent){
+    this.splat=[];
+		this.color= color(random(colori))
+    this.x=x;
+    this.y=y;
+		this.death=200;
+    this.noiseStart=random(10);
+  for(let i=this.noiseStart;i<this.noiseStart+TWO_PI; i+=0.1){
+    let acc= (noise(i));
+    this.splat.push(new pointer(i, acc, extent));
+  }  
   }
   
- 
-} 
+ move(){
+   for(let n of this.splat){
+     n.move();
+   }
+	 this.death-=1;
+   if(this.death<1){
+     let index = pioggia.indexOf(this);
+      pioggia.splice(index, 1);
+   }
+ } 
 
 
 
-
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+ show(){
+ noStroke();
+	 this.color.setAlpha(80);
+ fill(this.color);
+   push();
+  translate(this.x, this.y);
+  beginShape();
+  for(let i=0; i<this.splat.length; i++){
+    curveVertex(this.splat[i].pos.x, this.splat[i].pos.y)
+  }
+  endShape(CLOSE);
+pop() 
+ }
+  
 }
-
-
- 
